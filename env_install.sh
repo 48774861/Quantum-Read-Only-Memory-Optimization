292ced5a1c5c8ee58250373fcf2b26d4075ccb88
48774861 <112006435+48774861@users.noreply.github.com>
2026-09-25 11:08:07 -0500

    Worked on the estimation equation

--- env_install.sh
+++ env_install.sh
@@ -1,12 +1,14 @@
-conda activate quantum
-which python
-# Should be /opt/anaconda/envs/quantum/bin/python
+# In a normal terminal
+conda create -n quantum python=3.12.2
 
 python -m ipykernel install --user --name quantum --display-name "Python (quantum)"
 # Should see Installed kernelspec quantum
 jupyter kernelspec list
 # Should list quantum as a kernel.
 
+# Within the quantum conda environment
+conda activate quantum
+which python # Should be /opt/anaconda/envs/quantum/bin/python
 python -m pip install --upgrade pip
 python -m pip install pennylane
 python -m pip install matplotlib
